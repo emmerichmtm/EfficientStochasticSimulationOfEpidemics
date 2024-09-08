ea46976adc3b30df13960cbb99d01f4b6aaea86a
Michael Emmerich <58847423+emmerichmtm@users.noreply.github.com>
2024-09-08 07:14:34 +0300

    SI Process with numba and logger. Tested on small example

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import numpy as np
+from numba import njit
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
 from sortedcontainers import SortedList
 import logging
 
-# Set up logging to replace print statements, change level to DEBUG for detailed output
+# Set up logging
 logging.basicConfig(level=logging.INFO)
 
 class EpidemicGraph:
@@ -12,7 +14,7 @@ class EpidemicGraph:
         self.G = nx.Graph()
         self.infection_rate = infection_rate
         self.infected_nodes = SortedList(key=lambda x: -self.G.nodes[x]['sum_of_weights_i'])
-        self.total_weight = 0
+        self.total_rate = 0
 
     def add_node(self, node_id):
         self.G.add_node(node_id, infected=False, sum_of_weights_i=0.0)
@@ -21,52 +23,38 @@ class EpidemicGraph:
         self.G.add_edge(node1, node2, weight=weight)
 
     def simulate_step(self):
-        if not self.infected_nodes or self.total_weight <= 0:
+        if not self.infected_nodes or self.total_rate <= 0:
             return float('inf')
-        wait_time = random.expovariate(self.infection_rate * self.total_weight)
-        r = random.uniform(0, self.total_weight)
+        wait_time = random.expovariate(self.infection_rate * self.total_rate)
+        r = random.uniform(0, self.total_rate)
+        logging.info(f'Random threshold: {r}, Total weight: {self.total_rate}, Infection rate: {self.infection_rate}')
         cumulative = 0
-        # logging r total_weight and infection rate
-        logging.info(f'Random threshold: {r}, Total weight: {self.total_weight}, Infection rate: {self.infection_rate}')
-#
         for node in self.infected_nodes:
             cumulative += self.G.nodes[node]['sum_of_weights_i']
             logging.info(f'Node: {node}, Cumulative: {cumulative}')
             if cumulative > r:
                 self.infect_neighbor(node)
                 break
-
-        logging.debug(f'Total weight: {self.total_weight}, Rate: {self.infection_rate}, Random threshold: {r}')
+        logging.debug(f'Total weight: {self.total_rate}, Rate: {self.infection_rate}, Random threshold: {r}')
         return wait_time
 
     def infect_neighbor(self, infected_node):
         neighbors = [n for n in self.G.neighbors(infected_node) if not self.G.nodes[n]['infected']]
-        weights = [self.G[infected_node][n]['weight'] for n in neighbors]
-        total_neighbor_weight = sum(weights)
-        logging.info(f'Infected node: {infected_node}, Neighbors: {neighbors}, Weights: {weights}, Total weight: {total_neighbor_weight}')
-        if total_neighbor_weight > 0:
-            r = random.uniform(0, total_neighbor_weight)
-            cumulative = 0
-            for neighbor, weight in zip(neighbors, weights):
-                cumulative += weight
-                if cumulative > r:
-                    self.infect_node(neighbor)
-                    logging.info(f'Infecting neighbor: {neighbor}')
-                    break
+        if neighbors:
+            weights = np.array([self.G[infected_node][n]['weight'] for n in neighbors])
+            neighbor_to_infect = choose_neighbor_to_infect(weights)
+            self.infect_node(neighbors[neighbor_to_infect])
 
     def infect_node(self, node):
         self.G.nodes[node]['infected'] = True
         self.infected_nodes.add(node)
         for neighbor in self.G.neighbors(node):
-            logging.info(f'Neighbor: {neighbor}, Infected: {self.G.nodes[neighbor]["infected"]}')
             if not self.G.nodes[neighbor]['infected']:
                 self.G.nodes[node]['sum_of_weights_i'] += self.G[node][neighbor]['weight']
-                self.total_weight += self.G[node][neighbor]['weight']
-                logging.info(f'++Neighbor: {neighbor}, Weight: {self.G[node][neighbor]["weight"]}, Sum of weights: {self.G.nodes[node]["sum_of_weights_i"]}')
+                self.total_rate += self.G[node][neighbor]['weight']
             else:
                 self.G.nodes[neighbor]['sum_of_weights_i'] -= self.G[node][neighbor]['weight']
-                self.total_weight -= self.G[node][neighbor]['weight']
-                logging.info(f'--Neighbor: {neighbor}, Weight: {self.G[node][neighbor]["weight"]}, Sum of weights: {self.G.nodes[neighbor]["sum_of_weights_i"]}')
+                self.total_rate -= self.G[node][neighbor]['weight']
 
     def plot_graph(self, title="Graph"):
         plt.figure(figsize=(8, 5))
@@ -76,6 +64,17 @@ class EpidemicGraph:
         nx.draw_networkx_edges(self.G, pos, width=1.0, alpha=0.5)
         plt.title(title)
         plt.show()
+
+# Function optimized with Numba for choosing the neighbor to infect
+@njit
+def choose_neighbor_to_infect(weights):
+    total_weight = np.sum(weights)
+    r = random.uniform(0, total_weight)
+    cumulative = 0
+    for i in range(len(weights)):
+        cumulative += weights[i]
+        if cumulative > r:
+            return i
 
 # Example Usage
 graph = EpidemicGraph(infection_rate=0.1)
